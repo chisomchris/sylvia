a2624ba8bf085da73e119f52e0a6ea966dc65772
CHRISTIAN EZEANYAECHE <christianezeanyaeche@gmail.com>
2023-09-15 11:49:10 +0100

    Update Card.jsx

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -5,6 +5,7 @@ import { getGenreNames } from '../../utils/genre'
 export const Card = ({ poster, title, rating, likes, start, tags, id }) => {
     return (
         <div className="shadow-md max-w-[380px]" data-testid='movie-card'>
+            <Link href={`/movies/${id}`} className='hover:shadow-lg hover:shadow-pink-400'>
             <div className="bg-gray-500 relative">
                 <div className="flex absolute w-full p-6 items-center">
                     <button className="ml-auto bg-[#eeeeee80] h-[2.75rem] w-[2.75rem] rounded-full inline-grid place-items-center"><FaHeart className="text-2xl text-[#fefefe]" /></button>
@@ -13,7 +14,7 @@ export const Card = ({ poster, title, rating, likes, start, tags, id }) => {
             </div>
             <div className="py-6 px-4">
                 <p className="text-[#9CA3AF]">Released: <span data-testid='movie-release-date'>{start.slice(0, 4)}</span></p>
-                <Link href={`/movies/${id}`} className='hover:text-pink-500'><h2 className="font-semibold text-lg my-3" data-testid='movie-title'>{title}</h2></Link>
+                <h2 className="font-semibold text-lg my-3" data-testid='movie-title'>{title}</h2>
                 <div className="flex item-center justify-between pb-3">
                     <div className="flex items-center gap-3">
                         <Image src={'/imdb.jpg'} alt="" width={100} height={40} className="h-7 w-auto" /><p>{rating}</p>
@@ -29,6 +30,7 @@ export const Card = ({ poster, title, rating, likes, start, tags, id }) => {
                 </div>
                 <p className="text-[#9CA3AF]">{getGenreNames(tags).join(", ")}</p>
             </div>
+            </Link>
         </div>
     )
 }
